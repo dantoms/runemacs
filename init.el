e6687276d6e1957b2fb06b6d23836f1a208b2673
Dan Toms <157254317+pynmash@users.noreply.github.com>
2025-10-14 03:23:17 +0100

    Use smaller fringe to remove gap at sides

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
 (scroll-bar-mode -1)        ; Disable visible scrollbar
 (tool-bar-mode -1)          ; Disable the toolbar
 (tooltip-mode -1)           ; Disable tooltips
-(set-fringe-mode 10)        ; Give some breathing room
+(set-fringe-mode 1)        ; Give some breathing room
 
 (menu-bar-mode -1)            ; Disable the menu bar
 
